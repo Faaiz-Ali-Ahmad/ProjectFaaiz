4b8ac4ee78c6d607d50668fee2768f1d0c12f153
Faaiz Ali Ahmad <ahmafaai@sheridancollege.ca>
2026-02-05 18:03:47 -0500

    Stage Commit

--- src/main/java/week5/projectfaaiz/ProjectFaaiz.java
+++ src/main/java/week5/projectfaaiz/ProjectFaaiz.java
@@ -13,4 +13,7 @@ public class ProjectFaaiz {
     public static void main(String[] args) {
         System.out.println("Faaiz Ali Ahmad");
     }
+     public static void main1(String[] args) {
+        System.out.println("Hello World!! Faaiz Ali Ahmad");
+    }
 }
