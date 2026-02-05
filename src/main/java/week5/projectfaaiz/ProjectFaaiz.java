fb5d8c7884f0f8b341a378f6d8e54d9b7c9a5feb
Faaiz Ali Ahmad <ahmafaai@sheridancollege.ca>
2026-02-05 17:58:23 -0500

    First Commit!!

---
+++ src/main/java/week5/projectfaaiz/ProjectFaaiz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package week5.projectfaaiz;
+
+/**
+ *
+ * @author faaiz
+ */
+public class ProjectFaaiz {
+
+    public static void main(String[] args) {
+        System.out.println("Faaiz Ali Ahmad");
+    }
+}
